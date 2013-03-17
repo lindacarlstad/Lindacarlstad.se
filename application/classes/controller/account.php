10b64498a1c0ca5e94c6ede7a214c435bc1eeba5
hrr <jia@lekstuga.info>
2013-03-17 23:13:07 +0100

    Registrering med betalning tillagt.

--- application/classes/controller/account.php
+++ application/classes/controller/account.php
@@ -1,10 +1,166 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Account extends Controller {
+	protected $session; 
 	public function action_logout()
 	{		
 		AUTH::instance()->logout();
 		$this->request->redirect();
+	}
+	
+	public function action_register_pay()
+	{
+		if (AUTH::Instance()->get_user() == NULL)
+		{
+			if (isset($_POST['redirect']) && !empty($_POST['redirect']) && $_POST['redirect'] == 'redirect')
+			{
+				$this->session = Session::instance();
+								
+				if (isset($_POST['email']) && !empty($_POST['email']) &&
+					isset($_POST['name']) && !empty($_POST['name']) &&
+					isset($_POST['surname']) && !empty($_POST['surname']) &&
+					isset($_POST['password']) && !empty($_POST['password']) &&
+					isset($_POST['password_confirm']) && !empty($_POST['password_confirm']) &&
+					$_POST['password'] == $_POST['password_confirm'])
+				{
+					$requestParams = array(
+					   'RETURNURL' => 'http://lindacarlstad.se/account/register_success',
+					   'CANCELURL' => 'http://lindacarlstad.se/account/register_cancelled'
+					);
+					
+					$orderParams = array(	
+					   'PAYMENTREQUEST_0_AMT' => '1',
+					   'PAYMENTREQUEST_0_SHIPPINGAMT' => '0',
+					   'PAYMENTREQUEST_0_CURRENCYCODE' => 'SEK',
+					   'PAYMENTREQUEST_0_ITEMAMT' => '1'
+					);
+					$items = array(
+						   "L_PAYMENTREQUEST_0_NAME0" => 'Medlemsavgift',
+						   "L_PAYMENTREQUEST_0_AMT0" => '1',
+						   "L_PAYMENTREQUEST_0_QTY0" => '1'
+						);
+						
+					
+					$paypal = new Paypal();
+					$response = $paypal -> request('SetExpressCheckout',$requestParams + $orderParams + $items + array("SOLUTIONTYPE" => 'Sole'));
+					if(is_array($response) && $response['ACK'] == 'Success') 
+					{						
+						//Temp registrering, fult men hur fan gör man annars?
+						$this->session->set('reg0', $_POST['email']);
+						$this->session->set('reg1', $_POST['name']);
+						$this->session->set('reg2', $_POST['surname']);
+						$this->session->set('reg3', $_POST['password']);
+						$this->session->set('reg4', $response['TOKEN']);
+						  
+						$this->response->body(View::factory('account/account_register_pay')
+							->bind('errors', $errors)
+							->bind('values', $values)
+							->bind('token', $response['TOKEN']));	
+					}
+					else
+					{
+						$_POST['redirect'] = "";
+						$errors = "Kan inte koppla upp mot Paypal för tillfället!";
+						$this->response->body(View::factory('account/account_register_pay')
+						->bind('errors', $errors)
+						->bind('values', $values)
+						);
+					}
+				}
+				elseif ($_POST['password'] != $_POST['password_confirm']) 
+				{
+					$_POST['redirect'] = "";
+					$errors = "Lösenorden stämmer inte överens!";
+					$this->response->body(View::factory('account/account_register_pay')
+					->bind('errors', $errors)
+					->bind('values', $values)
+					);
+				}
+				else
+				{
+					$_POST['redirect'] = "";
+					$errors = "Något fält är tomt!";
+					$this->response->body(View::factory('account/account_register_pay')
+					->bind('errors', $errors)
+					->bind('values', $values));
+				}
+			}
+			else
+			{
+				$_POST['redirect'] = "";
+				$errors = "";
+				$this->response->body(View::factory('account/account_register_pay')
+				->bind('errors', $errors)
+				->bind('values', $values));
+			}
+		}
+		else
+		{
+			$this->request->redirect('/news/');
+		}
+	}
+	
+	public function action_register_success()
+	{
+		$this->session = Session::instance();
+		if(
+			isset($_GET['token']) && !empty($_GET['token']) && 
+			isset($_GET['PayerID']) && !empty($_GET['PayerID']) &&
+			strcmp($this->session->get('reg4'), $_GET['token']) == 0
+			)
+		{
+			$regArray = array('email' => $this->session->get('reg0'), 
+			'name' => $this->session->get('reg1'), 
+			'surname' => $this->session->get('reg2'), 
+			'password' => $this->session->get('reg3'));
+			$user = ORM::factory('user')->values($regArray, array('email', 'name', 'surname', 'password'));
+			try {
+					$user->check();
+					$user->save();
+					
+					$role = ORM::Factory('role', 1);
+					$user->add('roles', $role);
+	
+					AUTH::Instance()->force_login($user);
+					$this->request->redirect('/account/registered/');
+					session_destroy();
+			}
+			catch(ORM_Validation_Exception $e)
+			{
+				$errors = $e->errors('register');
+      			$values = $_POST;
+			}
+		}/*
+		$errors[] = $this->session->get('reg4');
+		$errors[] = $_GET['token'];
+		$errors[] = strcmp($this->session->get('reg4'), $_GET['token']);
+		$errors[] = isset($_GET['token']) && !empty($_GET['token']);
+		$errors[] = isset($_GET['PayerID']) && !empty($_GET['PayerID']);
+		 */
+		$this->response->body(View::factory('account/account_registerDebug')
+				 	->bind('errors', $errors)
+					->bind('values', $values)
+					->bind('regArray', $regArray));	
+		 //DEBUG
+		 /*
+		 $this->session = Session::instance();
+		 $regArray = array($this->session->get('reg0'), 
+		 $this->session->get('reg1'), 
+		 $this->session->get('reg2'), 
+		 $this->session->get('reg3'), 
+		 $this->session->get('reg4'));
+		 
+		 $tempId = $this->session->id();
+		 
+		 $this->response->body(View::factory('account/account_registerDebug')
+		 	->bind('regArray', $regArray)
+			->bind('sessionId', $tempId));	
+		  */
+	}
+	
+	public function action_register_cancelled()
+	{
+		$this->template->content = View::factory('account/account_register_cancelled');
 	}
 	
 	public function action_register()
---
+++ application/views/account/account_register_cancelled.php
@@ -0,0 +1,4 @@
+Köpet avbröts!
+<form method='post' action='/account/register_pay'>
+	<input type='submit' value='Tillbaka till registreringen'>
+</form>
